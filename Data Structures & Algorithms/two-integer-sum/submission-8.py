c89c4449b189718b744f532a63aafb348c456da8
Nikhil Upadhyay <54057879+nikhilupadhyay26@users.noreply.github.com>
2026-06-17 23:29:23 +0530

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        l, r = 0, len(nums)-1
+        nums1 = nums.copy()
+        nums.sort()
+        while l< r:
+            for i in range(0, len(nums)):
+                if nums[l]+nums[r] == target:
+                    return [nums1.index(nums[l]), nums1.index(nums[r])]
+                elif nums[l]+nums[r] < target:
+                    l = l+1
+                elif nums[l]+nums[r] > target:
+                    r = r-1
+            return [nums1.index(nums[l]), nums1.index(nums[r])]
+            
