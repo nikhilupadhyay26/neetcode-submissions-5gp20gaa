3b5eb0cecddd2630fe7c426db1006ad04b6eebbe
Nikhil Upadhyay <54057879+nikhilupadhyay26@users.noreply.github.com>
2026-07-02 23:26:31 +0530

    Add: string-encode-and-decode - submission-11

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-11.py
@@ -0,0 +1,22 @@
+class Solution:
+    def encode(self, strs):
+        s = ''
+        for i in strs:
+            s = s + str(len(i)) + '#' + i
+        return s
+
+    def decode(self, s: str):
+        res = []
+
+        j = 0
+        while j < len(s):
+            print('j', j)
+            hash_pos = s.find('#', j)
+            length = int(s[j:hash_pos])
+            print('length', length)
+            word = s[j+2:j+2+length]
+            print('word', word)
+            res.append(word)
+            print('res', res)
+            j = j + 2 + length
+        return res
